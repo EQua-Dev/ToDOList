e673735edef87ce7fdc9f47702ef6e2bcca4c5c8
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2023-06-25 15:11:16 +0100

    implemented saving todo items to db done

--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -6,7 +6,8 @@
 //
 
 import Foundation
-
+import FirebaseAuth
+import FirebaseFirestore
 
 class NewItemViewViewModel: ObservableObject{
     
@@ -17,6 +18,31 @@ class NewItemViewViewModel: ObservableObject{
     init(){}
     
     func save(){
+        guard canSave else{
+            return
+        }
+        
+        //get current user ID
+        guard let uId = Auth.auth().currentUser?.uid else{
+            return
+        }
+        
+        //create model
+        let newId = UUID().uuidString
+        let newItem = TodoListItem(id: newId,
+                                   title: title,
+                                   dueDate: dueDate.timeIntervalSince1970,
+                                   createdTime: Date().timeIntervalSince1970,
+                                   isDone: false)
+        
+        
+        //save model
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
         
     }
     
